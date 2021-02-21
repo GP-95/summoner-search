f5f8096ac3cc9a782f322760deeb1f52ffc8c78c
Robin Soosalu <robin.soosalu@gmail.com>
2021-02-22 00:46:04 +0200

    Added file extensions for Vercel deployment

--- components/SummonerCard.jsx
+++ components/SummonerCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../styles/SummonerCard.module.css'
 import Image from 'next/image'
 
-import Rank from '../components/Rank'
+import Rank from '../components/Rank.jsx'
 
 function SummonerCard({ summoner, rank }) {
   return (
